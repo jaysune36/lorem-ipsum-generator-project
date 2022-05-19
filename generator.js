9e455b17a6fdb5425684b213b024f57082341eeb
user <user@users-MacBook-Pro.local>
2022-05-18 19:52:14 -0700

    working on creating a different approach for the while loop to generate sentences and paragraph to be pushed to the main ipsumValue array. If successful will replace previous loop in word iput selection and make standard for all other input methods.

--- generator.js
+++ generator.js
@@ -31,12 +31,20 @@ const regExEnd = new RegExp('[.]');
 const ipsumText =
   `Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`;
 
+function randomNumber() {
+  return Math.floor(Math.random() * (12 - 3) + 3);
+}
+
+function randomNumber2() {
+  return Math.floor(Math.random() * (6 - 1) + 1);
+}
+
 function ispumGenerator(quantity) {
   let lengthType = form.querySelector('input[name="length-type"]:checked').value;
-  quantity = form.querySelector('input[type="text"]').value;
 
   let ipsumSplit = ipsumText.split(' ');
-  let randomSentLength = Math.floor(Math.random() * (12 - 3) + 3);
+  let ipsumSent = [];
+  let ipsumPar = [];
 
   let ipsumCap = ipsumSplit.filter(caps => {
     if(caps.match(regExCaps)) {
@@ -50,23 +58,66 @@ function ispumGenerator(quantity) {
     }
   });
 
+  let ipsumFiller = ipsumSplit.filter(filler => {
+    if(!filler.match(regExCaps)) {
+      if(!filler.match(regExEnd)) {
+        return filler;
+      }
+    }
+  })
+
+
   if(lengthType === 'word') {
+    let randomSentLength = randomNumber();
     while (ipsumValue.length < quantity) {
       let randomIpsum = ipsumSplit[Math.floor(Math.random() * ipsumSplit.length)];
       if(ipsumValue.length === 0 || 
         ipsumValue[ipsumValue.length - 1].includes('.')){
         ipsumValue.push(ipsumCap[Math.floor(Math.random() * ipsumCap.length)]);
+      } else if(ipsumValue.length === quantity - 1 || ipsumValue.length == randomSentLength - 1) {
+        ipsumValue.push(ipsumEnd[Math.floor(Math.random() * ipsumEnd.length)]);
+        randomSentLength = randomNumber();
+        console.log(ipsumValue.length == randomSentLength);
       } else {
         ipsumValue.push(randomIpsum);
-      }
-      if(ipsumValue.length === quantity - 1 || ipsumValue.length === randomSentLength) {
-        ipsumValue.push(ipsumEnd[Math.floor(Math.random() * ipsumEnd.length)])
       }
     }
 
   console.log(ipsumValue.join(' '));
 
   } else if (lengthType === 'paragraph') {
+
+    let randomSentLength = randomNumber();
+    let randomParLength = randomNumber2();
+    if(ipsumValue.length !== quantity) {
+    while (ipsumValue.length < quantity) {
+      if(ipsumSent.length === 0){
+        ipsumSent.push(ipsumCap[Math.floor(Math.random() * ipsumCap.length)]);
+      } else {
+        ipsumSent.push(ipsumFiller[Math.floor(Math.random() * ipsumFiller.length)]);
+      } 
+      
+      if(ipsumSent.length === randomSentLength - 1) {
+        ipsumSent.push(ipsumEnd[Math.floor(Math.random() * ipsumEnd.length)]);
+        ipsumPar.push(ipsumSent.join(' '));
+        console.log(ipsumPar);
+        ipsumSent = [];
+        if(ipsumPar.length === randomParLength - 1) {
+          ipsumValue.push(ipsumPar.join(' '));
+          ipsumPar = [];
+        }
+    }
+  }
+  } else {
+    console.log(ipsumValue.join(' '));
+    console.log(ipsumValue)
+
+  }
+
+    // if (ipsumValue.length === quantity) {
+    //   console.log(ipsumValue.join(' '));
+    //   ipsumValue = [];
+    // }
 
   } else if (lengthType === 'list') {
 
@@ -78,5 +129,7 @@ function ispumGenerator(quantity) {
 } 
 
 submitBtn.addEventListener('click', (e) => {
-  ispumGenerator(8);
+
+  let quantity = form.querySelector('input[type="text"]').value;
+  ispumGenerator(quantity);
 })
